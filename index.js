e57dc63ec521e8455e7dcc10af8df9d02db471d6
Sandy Yeung <sandynism@gmail.com>
2017-09-18 23:11:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function addToCart(item) {
   var price = Math.floor(Math.random() * 100)
   var thingies = Object.assign({}, {[item] : price})
   cart.push(thingies)
+  console.log(`${item} has been added to your cart.`)
 }
 
 function viewCart() {
